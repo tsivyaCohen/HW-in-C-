9d5128d77f114c08aa392986f8fc63f5d5f1fff1
tsivya-web <seminar@DOMAIN.ORG>
2025-03-04 10:57:45 +0200

    up my application in git

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplication1.Controllers
+{
+   
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    { 
+        static List<Product> products = new List<Product>();
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            return products.FirstOrDefault(u=>u.Id==id);
+
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public Product Post([FromBody] Product value)
+        {
+            products.Add(value);
+            return value;
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public Product Put(int id, [FromBody] Product value)
+        {
+            int index=products.FindIndex(u=>u.Id==id);
+            products[index].Name= value.Name;
+            products[index].Description= value.Description;
+            products[index].CategoryProduct = value.CategoryProduct;
+            return products[index];
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public Product Delete(int id)
+        {
+            int index = products.FindIndex(u => u.Id == id);
+            products.RemoveAt(index);
+            return products[index];
+        }
+    }
+}
